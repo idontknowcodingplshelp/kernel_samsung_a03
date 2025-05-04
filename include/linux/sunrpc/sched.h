7976181e5758919fcd42b5e84a1f452c6ccc4850
idkcodingplshelp <behaiquy@gmail.com>
2025-05-04 13:06:49 +0700

    fixup

--- include/linux/sunrpc/sched.h
+++ include/linux/sunrpc/sched.h
@@ -17,6 +17,9 @@
 #include <linux/wait_bit.h>
 #include <linux/workqueue.h>
 #include <linux/sunrpc/xdr.h>
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
+struct task_struct *simple_lmk_next;
+#endif
 
 /*
  * This is the actual RPC procedure call info.
